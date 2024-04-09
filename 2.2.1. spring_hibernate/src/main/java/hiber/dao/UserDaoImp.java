ffa6bf5971d5f603f9fa59608ebc19c51ddd4169
ViacheslavChikin <slv3211@gmail.com>
2024-04-09 23:38:09 +0300

    done

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -11,19 +11,33 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
 
+    @Override
+
+    public User getUserByCarIdAndModel(String model, int series) {
+
+        List<User> users  = sessionFactory.getCurrentSession()
+                .createQuery("from User where car = (from Car where model = ?1 and series = ?2)", User.class)
+                .setParameter(1, model)
+                .setParameter(2, series)
+                .getResultList();
+        if (!users.isEmpty()) {
+            return users.get(0);
+        }
+        return null;
+    }
 }
