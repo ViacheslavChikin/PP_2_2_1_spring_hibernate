63ec8667a4d6bb1dcc972e05956f2876ecc3655a
ViacheslavChikin <slv3211@gmail.com>
2024-04-11 15:05:04 +0300

    changed getUserByCarIdAndModel

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -29,15 +29,11 @@ public class UserDaoImp implements UserDao {
     @Override
 
     public User getUserByCarIdAndModel(String model, int series) {
-
-        List<User> users  = sessionFactory.getCurrentSession()
+        User user = sessionFactory.getCurrentSession()
                 .createQuery("from User where car = (from Car where model = ?1 and series = ?2)", User.class)
                 .setParameter(1, model)
                 .setParameter(2, series)
-                .getResultList();
-        if (!users.isEmpty()) {
-            return users.get(0);
-        }
-        return null;
+                .getSingleResult();
+        return user;
     }
 }
